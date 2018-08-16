ae6b720c6d84c306a76251d8d816ba8717204b88
Rich Harris <richard.a.harris@gmail.com>
2018-08-16 13:43:53 -0400

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -2,6 +2,10 @@ import * as assert from 'assert';
 import * as UNTITLED from '../src/index';
 
 describe('UNTITLED', () => {
+	it('returns hello world', () => {
+		assert.equal(UNTITLED.hello(), 'hello world!');	
+	});
+	
 	it('has tests', () => {
 		assert.ok(false);
 	});
